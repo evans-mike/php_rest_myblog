4e156078a285e612eadbd02f52f99fb2dd0fa35d
Mike Evans <mikeevans@Mikes-MBP.attlocal.net>
2020-04-24 15:45:37 -0400

    first stopping point to debug

--- api/post/read.php
+++ api/post/read.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
   // Headers
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
@@ -6,23 +6,23 @@
   include_once '../../config/Database.php';
   include_once '../../models/Post.php';
 
-  // Instantiate DB & connect
+  // INSTATNTIATE DB OBJECT
   $database = new Database();
   $db = $database->connect();
 
   // Instantiate blog post object
   $post = new Post($db);
 
-  // Blog post query
+  // blog post query
   $result = $post->read();
-  // Get row count
+  // get row count
   $num = $result->rowCount();
 
   // Check if any posts
   if($num > 0) {
     // Post array
     $posts_arr = array();
-    // $posts_arr['data'] = array();
+    $posts_arr['data'] = array();
 
     while($row = $result->fetch(PDO::FETCH_ASSOC)) {
       extract($row);
@@ -37,15 +37,14 @@
       );
 
       // Push to "data"
-      array_push($posts_arr, $post_item);
-      // array_push($posts_arr['data'], $post_item);
+      array_push($posts_arr['data'], $post_item);
     }
 
-    // Turn to JSON & output
+    // Turn to JSON and output
     echo json_encode($posts_arr);
 
   } else {
-    // No Posts
+    // No posts
     echo json_encode(
       array('message' => 'No Posts Found')
     );
--- config/Database.php
+++ config/Database.php
@@ -1,19 +1,19 @@
-<?php 
+<?php
   class Database {
-    // DB Params
+    // DB params
     private $host = 'localhost';
     private $db_name = 'myblog';
     private $username = 'root';
-    private $password = '';
+    private $password = '123456';
     private $conn;
 
-    // DB Connect
+    // DB connect
     public function connect() {
       $this->conn = null;
 
-      try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      try {
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname= ' . $this->db_name, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //lookup documentation for PDO
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
       }
