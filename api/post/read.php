4ce00770a27d730283da680588721f6d639f2503
Mike Evans <mikeevans@Mikes-MBP.attlocal.net>
2020-04-25 14:37:37 -0400

    Revert "first stopping point to debug"
    This reverts commit 4e156078a285e612eadbd02f52f99fb2dd0fa35d.

--- api/post/read.php
+++ api/post/read.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
   // Headers
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
@@ -6,23 +6,23 @@
   include_once '../../config/Database.php';
   include_once '../../models/Post.php';
 
-  // INSTATNTIATE DB OBJECT
+  // Instantiate DB & connect
   $database = new Database();
   $db = $database->connect();
 
   // Instantiate blog post object
   $post = new Post($db);
 
-  // blog post query
+  // Blog post query
   $result = $post->read();
-  // get row count
+  // Get row count
   $num = $result->rowCount();
 
   // Check if any posts
   if($num > 0) {
     // Post array
     $posts_arr = array();
-    $posts_arr['data'] = array();
+    // $posts_arr['data'] = array();
 
     while($row = $result->fetch(PDO::FETCH_ASSOC)) {
       extract($row);
@@ -37,14 +37,15 @@
       );
 
       // Push to "data"
-      array_push($posts_arr['data'], $post_item);
+      array_push($posts_arr, $post_item);
+      // array_push($posts_arr['data'], $post_item);
     }
 
-    // Turn to JSON and output
+    // Turn to JSON & output
     echo json_encode($posts_arr);
 
   } else {
-    // No posts
+    // No Posts
     echo json_encode(
       array('message' => 'No Posts Found')
     );
--- config/Database.php
+++ config/Database.php
@@ -1,19 +1,19 @@
-<?php
+<?php 
   class Database {
-    // DB params
+    // DB Params
     private $host = 'localhost';
     private $db_name = 'myblog';
     private $username = 'root';
-    private $password = '123456';
+    private $password = '';
     private $conn;
 
-    // DB connect
+    // DB Connect
     public function connect() {
       $this->conn = null;
 
-      try {
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname= ' . $this->db_name, $this->username, $this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //lookup documentation for PDO
+      try { 
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
       }
